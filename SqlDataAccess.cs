ac5d3c759ebdba08b7c929a767e5b284ed930e4c
sandygoreraza <gorerazasandy10@gmail.com>
2024-02-02 12:55:09 +0200

    Add files via upload

---
+++ SqlDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using MySqlConnector;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace DALLibrary
+{
+    public class SqlDataAccess
+    {
+        private readonly string _connectionString;
+
+        public SqlDataAccess(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters)
+        {
+            using (IDbConnection connection = new MySqlConnection(_connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void PersistData<T>(string sqlStatement, T parameters)
+        {
+            using (IDbConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
